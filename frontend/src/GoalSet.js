1e5f417780db29d143aa29c83453c46dd7c7a341
Jerry Hsu <Jerry>
2021-12-02 22:29:28 -0500

    Updated goals front form (miles, calories, time, goal timeline)

--- frontend/src/GoalSet.js
+++ frontend/src/GoalSet.js
@@ -9,6 +9,7 @@ import {
   Card,
   CardBody,
   CardTitle,
+  DatePicker,
   Form,
   FormGroup,
   Input,
@@ -30,15 +31,28 @@ function GoalSet({ createUserGoal }) {
   const handleSubmit = (e)=> {
     e.preventDefault();
 
-    for (let prop in formData) {
-      formData[prop] = parseInt( formData[prop] );
-    }
+    // for (let prop in formData) {
+    //   formData[prop] = parseInt( formData[prop] );
+    // }
     console.log('adding new goal');
     console.log(formData);
-    createUserGoal(formData);
+    // createUserGoal(formData);
     history.push("/");
   };
 
+  function getMonday(){
+    const today = new Date();
+    const dow = today.getDay();
+
+    const currWkMonday = today.getDate() - dow + (dow == 0 ? -6 : 1); 
+    const date = new Date(today.setDate(currWkMonday));
+    return date.toISOString().substr(0, 10);
+  }
+  // const newDate = new Date();
+  // // const currDate = newDate.getDate();
+  // const convertedDate = newDate.toISOString().substr(0, 10);
+
+  const convertedDate = getMonday()
   return (
     <>
       <div className="d-flex justify-content-center">
@@ -46,55 +60,75 @@ function GoalSet({ createUserGoal }) {
           <CardTitle>Create New Goal</CardTitle>
           <CardBody>
             <Form className="form" onSubmit={handleSubmit}>
-              <Label for="distance"></Label>
-              <Input
-                id="distance"
-                name="distance"
-                type="text"
-                placeholder="Distance"
-                autoComplete="on"
-                onChange={handleChange}
-              />
-              <Input
-                id="kilojoules"
-                name="kilojoules"
-                type="text"
-                placeholder="Kilojoules"
-                autoComplete="on"
-                onChange={handleChange}
-              />
-              <Input
-                id="movingTime"
-                name="movingTime"
-                type="text"
-                placeholder="Moving Time"
-                autoComplete="on"
-                onChange={handleChange}
-              />
               <FormGroup>
+                <legend>Goal</legend>
+                <Label for="miles"></Label>
                 <Input
-                  id="week"
-                  name="week"
-                  type="radio"
-                  onChange={handleChange}
-                />
-                <Label for="week">&nbsp; Week&nbsp;</Label>
-                <Input
-                  id="month"
-                  name="month"
-                  type="radio"
-                  onChange={handleChange}
-                />
-                <Label for="month">&nbsp; Month&nbsp;</Label>
-                <Input
-                  id="year"
-                  name="year"
-                  type="radio"
-                  placeholder="Time Period"
+                  id="miles"
+                  name="miles"
+                  type="text"
+                  placeholder="Distance (miles)"
                   autoComplete="on"
                   onChange={handleChange}
                 />
-                <Label for="year">&nbsp; Year</Label>
+                <Input
+                  id="calories"
+                  name="calories"
+                  type="text"
+                  placeholder="Calories"
+                  autoComplete="on"
+                  onChange={handleChange}
+                />
+                <Input
+                  id="time"
+                  name="time"
+                  type="text"
+                  placeholder="Time (format: hh:mm)"
+                  autoComplete="on"
+                  onChange={handleChange}
+                />
+              </FormGroup>
+              <FormGroup>
+                <legend>Goal Timeline</legend>
+                <FormGroup>
+                  <Label for="time-period">Select Time Period:&nbsp;</Label>
+                  <Input
+                    id="week"
+                    value="week"
+                    name="time-period"
+                    type="radio"
+                    onChange={handleChange}
+                  />
+                  <Label for="week">&nbsp; Week&nbsp;</Label>
+                  <Input
+                    id="month"
+                    value="month"
+                    name="time-period"
+                    type="radio"
+                    onChange={handleChange}
+                  />
+                  <Label for="month">&nbsp; Month&nbsp;</Label>
+                  <Input
+                    id="year"
+                    value="month"
+                    name="time-period"
+                    type="radio"
+                    placeholder="Time Period"
+                    autoComplete="on"
+                    onChange={handleChange}
+                  />
+                  <Label for="year">&nbsp; Year</Label>
+                </FormGroup>
+                <Label for="date">Goal Start Date:</Label>
+                <Input
+                  id="date"
+                  value="date"
+                  name="date"
+                  type="date"
+                  value={convertedDate}
+                  id="date"
+                  onChange={handleChange}
+                />
               </FormGroup>
               <Button color="primary" className="mt-1">
                 Set Goal!
