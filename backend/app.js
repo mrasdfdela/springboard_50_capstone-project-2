1ee516d1afefe613f0d721cc6415b91c159d025b
Jerry Hsu <Jerry>
2021-12-18 09:45:01 -0500

    deploying with update cors policy

--- backend/app.js
+++ backend/app.js
@@ -21,6 +21,15 @@ const bikeRoutes = require("./routes/bikes");
 const goalRoutes = require("./routes/goals");
 
 app.use(authenticateJWT);
+app.use(function (req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Methods", "GET,POST,DELETE,PATCH,PUT");
+  res.header(
+    "Access-Control-Allow-Headers",
+    "Origin, X-Requested-With, Content-Type, Accept"
+  );
+  next();
+});
 app.use("/auth", authRoutes);
 app.use("/users", userRoutes);
 app.use("/activities", actRoutes);
